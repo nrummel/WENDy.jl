fddc2a0416edf6dff2ad5c36f8e9e6ce8a39805b
Nic Rummel <nicrummel@gmail.com>
2024-06-21 18:13:59 -0600

    Change name to wendy

---
+++ src/WENDy.jl
@@ -0,0 +1,10 @@
+module WENDy
+# __precompile__(false)
+include("exampleProblems.jl")
+include("computeGradients.jl")
+include("generateNoise.jl")
+include("interp1d.jl")
+include("testFunctions.jl")
+using Random 
+
+end # module
