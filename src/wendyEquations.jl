6167f4f06dd4e34b7f02fe4b0388fb61b67159a7
Nic Rummel <nicrummel@gmail.com>
2024-07-04 00:04:56 -0600

    forgot to get the maholonobis distance

--- src/wendyEquations.jl
+++ src/wendyEquations.jl
@@ -14,9 +14,10 @@ function _L!(L::AbstractMatrix{<:Real}, U::AbstractMatrix{<:Real}, V::AbstractMa
         # @info "  $dt s, $a allocations"
         # @info " Compute L1 "
         # dt = @elapsed a = @allocations begin 
-            @avx for m in 1:M, d1 in 1:D, d2 in 1:D, k in 1:K
-                _Lbuff0[k,d2,d1,m] = JuF[d2,d1,m] * V[k,m] * sig[d1]
-            end
+            # @avx for m in 1:M, d1 in 1:D, d2 in 1:D, k in 1:K
+            #     _Lbuff0[k,d2,d1,m] = JuF[d2,d1,m] * V[k,m] * sig[d1]
+            # end # increases runtime by 1e-3 s
+            @tullio _Lbuff0[k,d2,d1,m] = JuF[d2,d1,m] * V[k,m]* sig[d1] # increases allocation from 4 to 45 
         # end
         # @info "  $dt s, $a allocations"
         # @info " add L0 "
@@ -125,12 +126,12 @@ function RTFun(prob::AbstractWENDyProblem, params::WENDyParameters, ::Val{T}=Val
     RTFun(prob.U, prob.V, prob.Vp, prob.sig, prob.jacuf!, params.diagReg, Val(T))
 end
 # method
-function (m::RTFun)(w::AbstractVector{W};ll::Logging.LogLevel=Logging.Warn) where W<:Real
+function (m::RTFun)(w::AbstractVector{W};ll::Logging.LogLevel=Logging.Warn, transpose::Bool=true) where W<:Real
     with_logger(ConsoleLogger(stderr, ll)) do 
         @info "++++ Res Eval ++++"
         L = m._L(w;ll=ll) 
         _R!(m.R, m.thisI, m.S, L, m.diagReg; ll=ll)
-        return m.R'
+        return transpose ? m.R' : m.R
     end
 end
 # function without struct declaration
@@ -313,133 +314,212 @@ function ∇res(RT::AbstractMatrix{<:Real}, U::AbstractMatrix{<:Real}, V::Abstra
     _∇res = ∇resFun(U, V, jacwf!, length(w), Val(T))
     _∇res(RT, w; ll=ll)
 end
-# ## Maholinobis distance 
-# struct MFun
-#     b0::AbstractVector{<:Real}
-#     _RT::RTFun
-#     _res::ResFun
-#     function MFun( U::AbstractMatrix, V::AbstractMatrix, Vp::AbstractMatrix, b0::AbstractVector, sig::AbstractVector, diagReg::Real, f!::Function, jacuf!::Function)
-#         _RT = RTFun(U, V, Vp, sig, jacuf!,diagReg)
-#         _res = ResFun(U, V, f!)
-#         new(b0, _RT, _res)
-#     end
-# end
+## Maholinobis distance 
+# struct
+struct MFun
+    b0::AbstractVector{<:Real}
+    _RT::RTFun
+    _res::ResFun
+end
+# constructors
+function MFun(U::AbstractMatrix, V::AbstractMatrix, Vp::AbstractMatrix, b0::AbstractVector, sig::AbstractVector, diagReg::Real, f!::Function, jacuf!::Function)
+    _RT = RTFun(U, V, Vp, sig, jacuf!,diagReg)
+    _res = ResFun(U, V, f!)
+    MFun(b0, _RT, _res)
+end
+function MFun(prob::AbstractWENDyProblem, params::WENDyParameters)
+    _RT = RTFun(prob, params)
+    _res = ResFun(prob, params)
+    MFun(prob.b0, _RT, _res)
+end
+# method
+function (m::MFun)(w::AbstractVector{W};ll::Logging.LogLevel=Logging.Warn) where W<:Real
+    # with_logger(ConsoleLogger(stderr, ll)) do 
+    #     @info "++++ Maholinobis Distance Eval ++++"
+    #     @info "Get RT"
+        # dt = @elapsed a = @allocations begin
+            RT = m._RT(w)
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info "Get b"
+        # dt = @elapsed a = @allocations begin
+            b = RT \ m.b0
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info "Get weighted residual"
+        # dt = @elapsed a = @allocations begin
+            r = m._res(RT,b,w)
+        # end
+        # @info "  $dt s, $a allocations"
+        return 1/2*norm(r)^2
+    # end
+end
+## Gradient of Maholinobis distance
+# raw function 
+function _∇m!(
+    ∇m::AbstractVector{<:Real},
+    U::AbstractMatrix{<:Real}, 
+    V::AbstractMatrix{<:Real}, 
+    Vp::AbstractMatrix{<:Real}, 
+    b0::AbstractVector{<:Real}, 
+    sig::AbstractVector{<:Real}, 
+    diagReg::AbstractFloat, 
+    _r::ResFun, 
+    _RT::RTFun, 
+    _∇r::∇resFun, 
+    jacwjacuf!::Function, 
+    JwJuF::AbstractArray{<:Real,4},
+    _Lbuf0::AbstractArray{<:Real,5},
+    _Lbuf1::AbstractArray{<:Real,5},
+    ∇Sw_buff::AbstractArray{<:Real,3},
+    ∇Sw::AbstractArray{<:Real,3}, 
+    b::AbstractVector{<:Real},
+    r::AbstractVector{<:Real},
+    rr::AbstractVector{<:Real},
+    w::AbstractVector{<:Real};
+    ll::Logging.LogLevel=Logging.Warn)
+    # with_logger(ConsoleLogger(stderr, ll)) do 
+        # @info "++++ ∇w[Maholinobis Dist] ++++"
+        D, M = size(U)
+        K, _ = size(V)
+        # @info " Compute R"
+        # dt = @elapsed a = @allocations begin 
+            R = _RT(w; transpose=false)
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Compute ∇w[∇uF] "
+        # dt = @elapsed a = @allocations begin 
+            @inbounds for m in 1:M
+                jacwjacuf!(view(JwJuF,:,:,:,m), w, view(U,:,m))
+            end
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Compute Lbuff0 "
+        # dt = @elapsed a = @allocations begin
+            @tullio _Lbuf0[k,d2,d1,j,m] = V[k,m] * JwJuF[d2,d1,j,m] * sig[d1] # increases allocations by 20
+            # @avx for m in 1:M, j in 1:J, d1 in 1:D, d2 in 1:D, k in 1:K
+            #     _Lbuf0[k,d2,d1,j,m] = V[k,m] * JwJuF[d2,d1,j,m] * sig[d1]
+            # end #increases runtime by .022 (2x slow down)
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Permutedims Lw "
+        # dt = @elapsed a = @allocations begin 
+            permutedims!(_Lbuf1, _Lbuf0, (1,2,5,3,4))
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Reshape Lw"
+        # dt = @elapsed a = @allocations begin 
+            ∇L = reshape(_Lbuf1,K*D,M*D,J)
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Compute ∇Sw(prt1)"
+        # dt = @elapsed a = @allocations begin 
+            # @tullio ∇Sw_buff[k1,k2,j] = ∇L[k1,m,j] * _RT._L.L[k2,m] # more than double the time and alloactions...
+            @inbounds for j = 1:J 
+                mul!(view(∇Sw_buff,:,:,j), view(∇L,:,:,j), _RT._L.L')
+            end
+        # end 
+        # @info "  $dt s, $a allocations"
+        # @info " Compute ∇Sw(prt2)"
+        # dt = @elapsed a = @allocations begin 
+            @tullio ∇Sw[kd1,kd2,j] = ∇Sw_buff[kd1,kd2,j] + ∇Sw_buff[kd2,kd1,j]
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Compute Residual"
+        # dt = @elapsed a = @allocations begin 
+            ldiv!(b, LowerTriangular(R'), b0)
+            _r(r, R', b, w)
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Applying R^{-1}"
+        # dt = @elapsed a = @allocations begin 
+            ldiv!(rr, UpperTriangular(R), r)
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Compute first part of ∇m"
+        # dt = @elapsed a = @allocations begin
+            @inbounds for j in 1:J 
+                ∇m[j] = -1/2*dot(rr, view(∇Sw,:,:,j), rr)
+            end 
+        # end
+        # @info "  $dt s, $a allocations"
+        # @info " Compute second part of ∇m"
+        # dt = @elapsed a = @allocations begin 
+            mul!(∇m, _∇r(R', w)', r, 1, 1)
+        # end
+        # @info "  $dt s, $a allocations"
+        nothing
+    # end
+end
 
-# function (m::MFun)(w::AbstractVector{W};ll::Logging.LogLevel=Logging.Warn) where W<:Real
-#     with_logger(ConsoleLogger(stderr, ll)) do 
-#         @info "++++ Maholinobis Distance Eval ++++"
-#         @info "Get RT"
-#         dt = @elapsed a = @allocations RT = m._RT(w)
-#         @info "  $dt s, $a allocations"
-#         @info "Get b"
-#         dt = @elapsed a = @allocations b = RT \ b0
-#         @info "  $dt s, $a allocations"
-#         @info "Get weighted residual"
-#         dt = @elapsed a = @allocations r = m._res(RT,b,w)
-#         @info "  $dt s, $a allocations"
-#         return 1/2*norm(r)^2
-#     end
-# end
+struct ∇mFun
+    U::AbstractMatrix{<:Real}
+    V::AbstractMatrix{<:Real}
+    Vp::AbstractMatrix{<:Real}
+    b0::AbstractVector{<:Real}
+    sig::AbstractVector{<:Real}
+    diagReg::Real
+    _r::ResFun
+    _RT::RTFun
+    _∇r::∇resFun
+    jacwjacuf!::Function
+    JwJuF::AbstractArray{<:Real,4} 
+    _Lbuf0::AbstractArray{<:Real,5} 
+    _Lbuf1::AbstractArray{<:Real,5} 
+    ∇Sw_buff::AbstractArray{<:Real,3} 
+    ∇Sw::AbstractArray{<:Real,3} 
+    b::AbstractVector{<:Real} 
+    r::AbstractVector{<:Real} 
+    rr::AbstractVector{<:Real} 
+    ∇m::AbstractVector{<:Real}
+end
+# constructor
+function ∇mFun(U::AbstractMatrix, V::AbstractMatrix, Vp::AbstractMatrix, b0::AbstractVector, sig::AbstractVector, diagReg::Real, _r::ResFun, _RT::RTFun, _∇r::∇resFun, jacwjacuf!::Function, J::Int, ::Val{T}=Val(Float64)) where T
+    D,M  = size(U)
+    K, _ = size(V)
+    JwJuF = zeros(T,D,D,J,M)
+    _Lbuf0 = zeros(T,K,D,D,J,M)
+    _Lbuf1 = zeros(T,K,D,M,D,J)
+    ∇Sw_buff = zeros(T,K*D,K*D,J)
+    ∇Sw = zeros(T,K*D,K*D,J)
+    b = zeros(T,K*D)
+    r = zeros(T,K*D)
+    rr = zeros(T,K*D)
+    ∇m = zeros(T,J)
+    ∇mFun(U,V,Vp,b0,sig,diagReg,_r,_RT,_∇r,jacwjacuf!,JwJuF,_Lbuf0,_Lbuf1,∇Sw_buff,∇Sw,b,r,rr,∇m)
+end
+function ∇mFun(prob::AbstractWENDyProblem, params::WENDyParameters, ::Val{T}=Val(Float64)) where T
+    _r = ResFun(prob, params, Val(T))
+    _RT = RTFun(prob, params, Val(T))
+    _∇r = ∇resFun(prob, params, Val(T))
+    ∇mFun(prob.U,prob.V,prob.Vp,prob.b0,prob.sig,params.diagReg,_r,_RT,_∇r,prob.jacwjacuf!, prob.J, Val(T))
+end
+# method
+# inplace
+function (m::∇mFun)(∇m::AbstractVector{<:Real}, w::AbstractVector{W}; ll::Logging.LogLevel=Logging.Warn) where W<:Real
+    _∇m!(∇m,m.U,m.V,m.Vp,m.b0,m.sig,m.diagReg,
+        m._r,m._RT,m._∇r,m.jacwjacuf!,
+        m.JwJuF,m._Lbuf0,m._Lbuf1,m.∇Sw_buff,m.∇Sw,m.b,m.r,m.rr,
+        w; ll=ll
+    )
+    nothing
+end
+# allocating
+function (m::∇mFun)(w::AbstractVector{W}; ll::Logging.LogLevel=Logging.Warn, allocate::Bool=false) where W<:Real
+    if allocate
+        ∇m = zeros(length(w))
+        _∇m!(∇m,m.U,m.V,m.Vp,m.b0,m.sig,m.diagReg,
+            m._r,m._RT,m._∇r,m.jacwjacuf!,
+            m.JwJuF,m._Lbuf0,m._Lbuf1,m.∇Sw_buff,m.∇Sw,m.b,m.r,m.rr,
+            w; ll=ll
+        )
+        return ∇m
+    end
+    _∇m!(m.∇m,m.U,m.V,m.Vp,m.b0,m.sig,m.diagReg,
+        m._r,m._RT,m._∇r,m.jacwjacuf!,
+        m.JwJuF,m._Lbuf0,m._Lbuf1,m.∇Sw_buff,m.∇Sw,m.b,m.r,m.rr,
+        w; ll=ll
+    )
+    return m.∇m
+end
 
-# struct ∇mFun!
-#     U::AbstractMatrix{<:Real}
-#     V::AbstractMatrix{<:Real}
-#     Vp::AbstractMatrix{<:Real}
-#     b0::AbstractVector{<:Real}
-#     sig::AbstractVector{<:Real}
-#     diagReg::Real
-#     f!::Function
-#     _L::LFun
-#     jacwf!::Function
-#     jacwjacuf!::Function
-#     JwJuF::AbstractArray{<:Real,4} 
-#     _Lbuf0::AbstractArray{<:Real,5} 
-#     _Lbuf1::AbstractArray{<:Real,5} 
-#     ∇Sw_buff::AbstractArray{<:Real,3} 
-#     ∇Sw::AbstractArray{<:Real,3} 
-#     function ∇mFun!(U::AbstractMatrix, V::AbstractMatrix, Vp::AbstractMatrix, b0::AbstractVector, sig::AbstractVector, diagReg::Real, f!::Function, jacuf!::Function, jacwf!::Function, jacwjacuf!::Function, J::Int, ::Val{T}=Val(Float64)) where T
-#         D,M  = size(U)
-#         K, _ = size(V)
-#         JwJuF = zeros(T,D,D,J,M)
-#         _Lbuf0 = zeros(T,K,D,D,J,M)
-#         _Lbuf1 = zeros(T,K,D,M,D,J)
-#         ∇Sw_buff = zeros(T,K*D,K*D,J)
-#         ∇Sw = zeros(T,K*D,K*D,J)
-#         _L = LFun(U,V,Vp,sig, jacuf!)
-#         new(U,V,Vp,b0,sig,diagReg,f!,_L,jacwf!,jacwjacuf!,JwJuF,_Lbuf0,_Lbuf1,∇Sw_buff,∇Sw)
-#     end
-# end
-
-# function (m::∇mFun!)(∇m::AbstractVector{<:Real}, w::AbstractVector{W}; ll::Logging.LogLevel=Logging.Warn) where W<:Real
-#     _∇m!(∇m,m.U,m.V,m.Vp,m.b0,m.sig,m.diagReg,
-#         m.f!,m._L,m.jacwf!,m.jacwjacuf!,
-#         m.JwJuF,m._Lbuf0,m._Lbuf1,m.∇Sw_buff,m.∇Sw,
-#         w; ll=ll
-#     )
-#     nothing
-# end
-# function _∇m!(U, V, Vp, b0, sig, diagReg, f!, _L, jacwf!, jacwjacuf!, JwJuF, _Lbuf0, _Lbuf1, ∇Sw_buff, ∇Sw, w;ll=Logging.Warn)
-#     with_logger(ConsoleLogger(stderr, ll)) do 
-#         @info "++++ ∇w[Maholinobis Dist] ++++"
-#         D, M = size(U)
-#         K, _ = size(V)
-#         @info "  $dt s, $a allocations"
-#         @info " Compute Lw"
-#         dt = @elapsed a = @allocations L = _L(w) 
-#         @info "  $dt s, $a allocations"
-#         @info " Get Cholesky of Sw"
-#         dt = @elapsed a = @allocations R = _R(L, diagReg)
-#         @info "  $dt s, $a allocations"
-#         @info " Compute ∇w[∇uF] "
-#         dt = @elapsed a = @allocations @inbounds for m in 1:M
-#             jacwjacuf!(view(JwJuF,:,:,:,m), w, view(U,:,m))
-#         end
-#         @info "  $dt s, $a allocations"
-#         @info " Compute Lbuff0 "
-#         dt = @elapsed a = @allocations begin
-#             @tullio _Lbuf0[k,d2,d1,j,m] = V[k,m] * JwJuF[d2,d1,j,m] * sig[d1]
-#         end
-#         @info "  $dt s, $a allocations"
-#         @info " Permutedims Lw "
-#         dt = @elapsed a = @allocations permutedims!(_Lbuf1, _Lbuf0, (1,2,5,3,4))
-#         @info "  $dt s, $a allocations"
-#         @info " Reshape Lw"
-#         dt = @elapsed a = @allocations ∇L = reshape(_Lbuf1,K*D,M*D,J)
-#         @info "  $dt s, $a allocations"
-#         @info " Compute ∇Sw(prt1)"
-#         @show typeof(∇Sw_buff)
-#         @show typeof(∇L)
-#         @show typeof(L)
-#         dt = @elapsed a = @allocations begin 
-#             @inbounds for j = 1:J 
-#                 mul!(view(∇Sw_buff,:,:,j), view(∇L,:,:,j), L')
-#             end
-#         end 
-#         @info "  $dt s, $a allocations"
-#         @info " Compute ∇Sw(prt2)"
-#         dt = @elapsed a = @allocations begin 
-#             @tullio ∇Sw[kd1,kd2,j] = ∇Sw_buff[kd1,kd2,j] + ∇Sw_buff[kd2,kd1,j]
-#         end
-#         @info "  $dt s, $a allocations"
-#         @info " Compute Residual"
-#         dt = @elapsed a = @allocations begin 
-#             b = R' \ b0
-#             r = res(R', U, V, b, f!, w)
-#         end
-#         @info "  $dt s, $a allocations"
-#         @info " Applying R^{-1}"
-#         dt = @elapsed a = @allocations rr = R \ r
-#         @info "  $dt s, $a allocations"
-#         @info " Compute first part of ∇m"
-#         dt = @elapsed a = @allocations begin
-#             @inbounds for j in 1:J 
-#                 ∇m[j] = -1/2*dot(rr, ∇Sw[:,:,j], rr)
-#             end 
-#         end
-#         @info "  $dt s, $a allocations"
-#         @info " Compute second part of ∇m"
-#         dt = @elapsed a = @allocations ∇m .+= ∇res(R',U,V,jacwf!,w)' * r
-#         @info "  $dt s, $a allocations"
-#         nothing
-#     end
-# end
--- test/compareInteremediateToMatlab.jl
+++ test/compareInteremediateToMatlab.jl
@@ -16,8 +16,6 @@ nothing
 ex = HINDMARSH_ROSE
 params = WENDyParameters(;noiseRatio=0.05)
 wendyProb = _MATLAB_WENDyProblem(ex, params;ll=Logging.Info)
-iter = NLS_iter(wendyProb, params)
-iterLin = Linear_IRWLS_Iter(wendyProb, params)
 wTrue = wendyProb.wTrue
 J = length(wTrue)
 w0 = wendyProb.data["w0"][:]
@@ -26,14 +24,14 @@ nothing
 Lw = LFun(wendyProb,params)
 @time Lw0 = Lw(w0)
 relErr = norm(Lw0 - wendyProb.data["Lw"]) /norm( wendyProb.data["Lw"])
-relErr < 1e-14 ? (@info "Lw in spec") : (@warn "Lw out of spec")
+relErr < eps()*1e2 ? (@info "Lw in spec (relerr = $relErr)") : (@warn "Lw out of spec (relerr = $relErr)")
 ## Sw/RT
 _RT = RTFun(wendyProb,params)
 @time RT0  = _RT(w0)
 relErr = norm(_RT.S - wendyProb.data["Sw"]) /norm( wendyProb.data["Sw"])
-relErr < 1e-14 ? (@info "Sw in spec") : (@warn "Sw out of spec")
+relErr < eps()*1e2 ? (@info "Sw in spec (relerr = $relErr)") : (@warn "Sw out of spec (relerr = $relErr)")
 relErr = norm(RT0 - wendyProb.data["RT"]) /norm( wendyProb.data["RT"])
-relErr < 1e-14 ? (@info "RT in spec") : (@warn "RT out of spec")
+relErr < eps()*1e2 ? (@info "RT in spec (relerr = $relErr)") : (@warn "RT out of spec (relerr = $relErr)")
 ## Residual
 RT_matlab = wendyProb.data["RT"]
 b_matlab = RT_matlab \ wendyProb.data["b_0"][:]
@@ -41,7 +39,7 @@ _res = ResFun(wendyProb, params)
 @time r0 = _res(RT_matlab, b_matlab, w0; ll=Logging.Info)
 r0_matlab = RT_matlab \ (wendyProb.data["G_0"] * w0) - b_matlab
 relErr = norm(r0 - r0_matlab) / norm( r0_matlab)
-relErr < eps()*1e2 ? (@info "r0 in spec") : (@warn "r0 out of spec")
+relErr < eps()*1e2 ? (@info "r0 in spec (relerr = $relErr)") : (@warn "r0 out of spec (relerr = $relErr)")
 ## jacobian of residual 
 # because the this ode is linear in paramets ∇_wr = G 
 G_matlab = RT_matlab \ wendyProb.data["G_0"]
@@ -50,9 +48,10 @@ jacFun = ∇resFun(wendyProb,params)
 Gtilde = jacFun(RT_matlab, rand(J))
 @assert norm(G-Gtilde) /norm(G) < eps()*1e2 
 relErr = norm(G - G_matlab) / norm(G_matlab)
-relErr < eps()*1e2 ? (@info "∇r in spec") : (@warn "∇r out of spec")
+relErr < eps()*1e2 ? (@info "∇r in spec (relerr = $relErr)") : (@warn "∇r out of spec (relerr = $relErr)")
 ## IRWLS
 @info "IRWLS linear"
+iterLin = Linear_IRWLS_Iter(wendyProb, params)
 dt = @elapsed a = @allocations what, wit, resit = IRWLS(
     wendyProb, params, iterLin, w0
 )
@@ -66,9 +65,10 @@ if typeof(what) <:AbstractVector
     @info "   coeff rel err = $relErr"
 end
 relErr = norm(wendyProb.data["w_hat_its"] - wit)/norm(wit)
-relErr < eps()*1e2 ? (@info "wits in spec with matlab") : (@warn "wits out of spec with matlab")
+relErr < eps()*1e2 ? (@info "wits in spec (relerr = $relErr) with matlab") : (@warn "wits out of spec (relerr = $relErr) with matlab")
 ##
 @info "IRWLS non-linear"
+iter = NLS_iter(wendyProb, params; maxiters=10)
 dt = @elapsed a = @allocations what, wit, resit = IRWLS(
     wendyProb, params, iter, w0;
     # ll=Logging.Info, iterll=Logging.Info, trueIter=iterLin
@@ -82,3 +82,15 @@ if typeof(what) <:AbstractVector
     relErr = norm(wit[:,end] - wTrue) / norm(wTrue)
     @info "   coeff rel err = $relErr"
 end
+## check Maholinobis distance 
+_m = MFun(wendyProb, params)
+@time m = _m(w0)
+m_matlab = wendyProb.data["m"][1]
+relErr = abs(2*m - m_matlab) / abs(m_matlab)
+relErr < eps()*1e2 ? (@info "m in spec (relerr = $relErr) with matlab") : (@warn "m out of spec (relerr = $relErr) with matlab")
+## check gradient of Maholinobis distance
+_∇m = ∇mFun(wendyProb, params)
+@time ∇m = _∇m(w0;ll=Logging.Info)
+∇m_matlab = wendyProb.data["gradm"][:]
+relErr = norm(2*∇m - ∇m_matlab) / norm(∇m_matlab)
+relErr < eps()*1e2 ? (@info "∇m in spec (relerr = $relErr) with matlab") : (@warn "∇m out of spec (relerr = $relErr) with matlab")
